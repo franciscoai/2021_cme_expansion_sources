bc9d7ab9e2b382f55e8e79350a363df56bd0e42c
Luchomerenda3 <lucianomerenda3@gmail.com>
2022-02-08 22:34:31 -0300

    Improved drms_download: added more flexibility to use it

--- tools/lu_tools.py
+++ tools/lu_tools.py
@@ -3,11 +3,11 @@ import gspread as gs #use google apis to read cmes info from google spreadsheet
 import os
 import drms
 
-
-#Random tools for wrking with solar physics data.
+#Random tools for working with solar physics data
+#Author: MerendaLucianoA.@GEHMe
 
 #read harpnums database
-harpnum_db= pd.read_csv("http://jsoc.stanford.edu/doc/data/hmi/harpnum_to_noaa/all_harps_with_noaa_ars.txt",sep=" ")
+harpnum_db = pd.read_csv("http://jsoc.stanford.edu/doc/data/hmi/harpnum_to_noaa/all_harps_with_noaa_ars.txt",sep=" ")
 
 def noaa2harpnum(noaa):
     #check the database of noaa vs harpnum and returns the harpnum associated with a noaa AR number.
@@ -15,7 +15,7 @@ def noaa2harpnum(noaa):
     harps = list(harpnum_db["HARPNUM"])
     harpnum_count = 0
     harpnuml = []
-    i = 0
+    i = 0 # aux variable.
 
     while i < len(noaas) - 1:
         if str(noaa) in noaas[i].split(sep=","):
@@ -31,10 +31,84 @@ def noaa2harpnum(noaa):
 
     return harpnum
 
-"Dates and Times CMEs"
+def drms_download(start_time,end_time,instrument="hmi",type="sharp",data_series="",segments="",user_email="lucianomerenda3@gmail.com"):
+
+    # vso type  client for downloading data from sdo and soho/mdi.
+    # Parameters:
+    #   "Time format for start_time and end_time is 2010.12.12_00:00:00_TAI"
+    #   instrument can mdi, hmi, aia,
+    #   type can be sharp, L1 depends on the instrument
+    #   segments depends on the type of data
+
+
+    # set the drms client
+    drms_client = drms.Client(email=user_email, verbose=True)
+
+    #Select data series
+    if data_series == "":
+        #print data series available of input instrument
+        available_series = drms_client.series(instrument.lower(),full=True)
+        print(f"Available data series for {instrument}:\n")
+        for series in available_series.index:
+            print(f"{series}) {available_series.name[series]} : {available_series.note[series]}")
+
+        ds_selected = available_series.name[int(input("\nSelect desired data series: "))]
+    else:
+        ds_selected = data_series
+
+    #Select segments of the data needed
+    if segments == "":
+        segments_available = drms_client.info(ds_selected).segments
+        for i,segment in enumerate(segments_available.index):
+            print(f"{i}) {segment}: {segments_available.note[segment]}")
+
+        segments_selected = "{" + input("Enter segments name separated by a comma: ") + "}"
+    else:
+        segments_selected = segments
+
+    #Check if we need a harpnum for the data series selected
+    if "HARPNUM" in drms_client.pkeys(ds_selected):
+        noaa_input = input("Enter NOAA number: ")
+        harpnum = "[" + noaa2harpnum(noaa_input) + "]" #only for sharps and smarps
+    else:
+        harpnum = ""
+
+    #Enter start and end time
+    time_window = f"[{start_time}-{end_time}]"
+
+    # Lets create the export request shall we?
+    ds_needed = ds_selected + harpnum + time_window + segments_selected
+    print("\nDRMS request: ds_needed\n")
+    export_request = drms_client.export(ds_needed, method='url', protocol='fits')
+
+    # Wait for the server to prepare requested files
+    export_request.wait()
+
+    # Then download files
+    # Set the download directory first
+    if "sharp" in ds_selected:
+        download_dir = "/gehme/data/sdo/hmi/sharps/" + start_time[0:10].replace(".","") + "/"
+    elif "smarp" in ds_selected:
+        download_dir = "/gehme/data/soho/mdi/smarps/" + start_time[0:10].replace(".","") + "/"
+    elif "aia.lev1" in ds_selected:
+        download_dir = "/gehme/data/sdo/aia/L1/wavelength/" + start_time[0:10].replace(".","") + "/"
+    elif "hmi.M" in ds_selected:
+        download_dir = "/gehme/data/sdo/hmi/" + start_time[0:10].replace(".", "") + "/"
+    else:
+        download_dir = input("Please manually enter the download directory\nRemember to use this convention! ---> /gehme/data/observatory/intrument/../data_type.../date/: \n")
+
+    print(f"Data will be downloaded in {download_dir}")
+    export_request.download(download_dir)
+
+
 
 def google_to_csv(gtitle,fname):
     #Import data from google spreadsheets and save in a csv file
+    #NOTE: before using, read gspread documentation on how to set up the google cloud app
+    #      in order to finally be able to use it with your google account
+    #IMPORTANT: Can't be used from within the server as far a I understand,
+    #           since autentication needs a browser GUI running in the server
+
     #using the gspread library
     savepath = os.getcwd() + "/" + fname
     #Start google client by auth Remember to put json auth file in /.config/gspread
@@ -45,47 +119,3 @@ def google_to_csv(gtitle,fname):
     dataframe = pd.DataFrame(worksheet.get_all_values())
 
     dataframe.to_csv(savepath, sep=",")
-
-def drms_download(user_email="lucianomerenda3@gmail.com",download_dir="/gehme/data/sdo/hmi/sharps/",instr="sharp"):
-
-    # set the drms client
-    drms_client = drms.Client(email=user_email, verbose=True)
-
-    #Select data series
-    #print data series available of input instrument
-    available_series = drms_client.series(instr.lower(),full=True)
-    print(f"Available data series for {instr}:\n")
-    for series in available_series.index:
-        print(f"{series}) {available_series.name[series]} : {available_series.note[series]}")
-
-    ds_selected = available_series.name[int(input("\nSelect desired data series: "))]
-
-    #segments of the data needed
-    segments_available = drms_client.info(ds_selected).segments
-    for i,segment in enumerate(segments_available.index):
-        print(f"{i}) {segment}: {segments_available.note[segment]}")
-
-    segments_selected = "{" + input("Enter segments name separated by a comma: ") + "}"
-
-    #Check if we need a harpnum for the data series selected
-    if "HARPNUM" in drms_client.pkeys(ds_selected):
-        noaa_input = input("Enter NOAA number: ")
-        harpnum = "[" + noaa2harpnum(noaa_input) + "]" #only for sharps and smarps
-    else:
-        harpnum = ""
-
-    #Enter start and end time
-    start_time = input("Enter start time (Time format example = 2010.12.12_00:00:00_TAI): ")
-    end_time = input("Enter end time (Same format as start time): ")
-    time_window = f"[{start_time}-{end_time}]"
-
-    # Lets create the export request shall we?
-    ds_needed = ds_selected + harpnum + time_window + segments_selected
-    print(ds_needed)
-    export_request = drms_client.export(ds_needed, method='url', protocol='fits')
-
-    # Wait for the server to prepare requested files
-    export_request.wait()
-
-    # Then download files
-    export_request.download(download_dir)
