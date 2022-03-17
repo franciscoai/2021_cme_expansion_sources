049601062f9820a7de42762bb3cb0aba8dd1932b
Gerardo <g.destefanis@alumno.um.edu.ar>
2022-03-17 12:55:04 -0300

    agregue un par de lineas de programa con el fin de plotear un punto en la imagen. Coordenadas en arcsec

--- tools/destefanis/plot_hmi.py
+++ tools/destefanis/plot_hmi.py
@@ -1,16 +1,28 @@
-def save_img(path,name):
+from tkinter.messagebox import NO
+import os
+
+def save_img(ifile,opath,point=None):
+    """
+    
+    :point: Optional point to plot in the image [x,y] in arcsecs. Default None
+    """
     import matplotlib.pyplot as plt
     import astropy.units as u
     import sunpy.data.sample
     import sunpy.map
     from astropy.io import fits
-    hmi_map= sunpy.map.Map(path)
+    from astropy.coordinates import SkyCoord
+    hmi_map= sunpy.map.Map(ifile)
     hmi_map.plot_settings['cmap'] = "hmimag"
     hmi_map.plot_settings['norm'] = plt.Normalize(-1500, 1500)
     fig = plt.figure(figsize=(6, 5))
     ax2 = fig.add_subplot(1, 1, 1, projection=hmi_map)
     hmi_map.plot(axes=ax2)
-    plt.savefig(name + '.png')
+    if point is not None: 
+        map_coord = (point * u.arcsec)
+        ax2.plot(map_coord[0].to('deg'), map_coord[1].to('deg'), 'o', color='white', transform=ax2.get_transform('world'))     
+        print(f'Map coordinate [{map_coord[0]}, {map_coord[1]}]')
+    plt.savefig(opath + "/" + os.path.basename(ifile) +".png")
     return
 
 
