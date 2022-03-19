b68c71a9d3c0398579ab809964aad70e791fbc40
Luchomerenda3 <lucianomerenda3@gmail.com>
2022-03-18 21:55:27 -0300

    Fixed a bug in noaa2harpnum. Created the final sharps download script after double checking cme source and start-end times on the Dates and Times CMEs google sheet

---
+++ tools/merenda/sharps_dowload.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import lu_tools as lt
+
+#Script para descargar los datos de sharps en el servidor
+
+#Tomamos los datos del google sheet "Dates and Times CMEs" y los refinames manualmente para obtener
+#los datos pertinentes a los sharps.
+sharp_data = pd.read_csv("/home/lugem/GEHMe/lucho_repo/2021_cme_expansion_sources/sharp_download.csv")
+
+#Parametros comunes para todas las descargas
+sharps_series = "hmi.sharp_cea_720s"
+sharp_segments = "Br,Br_err,Bp,Bp_err,Bt,Bt_err,bitmap,conf_disambig"
+
+for row in sharp_data.index:
+
+    #tomamos datos fila a fila para cada cme y generemos el download request.
+    inicio = sharp_data.loc[row,"Start-fecha"] + "_" + sharp_data.loc[row,"Start -Time"] + "_TAI"
+    fin = sharp_data.loc[row,"End-fecha"] + "_" + sharp_data.loc[row,"End - Time"] + "_TAI"
+    noaa_number = sharp_data.loc[row,"NOAA AR"]
+
+    lt.drms_download(start_time=inicio,end_time=fin,data_series=sharps_series,segments=sharp_segments,noaa=noaa_number)
+
+
+
+
+
+
+
+
+
