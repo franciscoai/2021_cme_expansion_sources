fixed arcade angle computation

tools/iglesias/arcades_prop.py
@@ -16,6 +16,7 @@ from turtle import color
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+mpl.use('Agg')
 import os
 from astropy.coordinates import SkyCoord
 import astropy.units as u
@@ -78,31 +79,33 @@ for f in files:
     # adds df['length3 [arcsec]'] to df['length2 [arcsec]'] where df['length3 [arcsec]'] is not nan, otherwise adds 0
     df['length2+3 [arcsec]'] = df.apply(lambda x: np.array([x['length2 [arcsec]'][i] + x['length3 [arcsec]'][i] if not np.isnan(x['length3 [arcsec]'][i]) else x['length2 [arcsec]'][i] for i in range(len(x['length2 [arcsec]']))]), axis=1)
 
-    # compute the arcade tilt angle using the mean GreatArc of all pairs of footpoints
+    # compute the arcade tilt angle by fitting a line to each group of points
     df['tilt mean [deg]'] = np.nan
     df['tilt sd [deg]'] = np.nan
     df['fp4'] = df.apply(lambda x: [SkyCoord(x['lon2 [arcsec]'][i], x['lat1 [arcsec]'][i], unit='arcsec',frame="heliographic_stonyhurst", observer="earth",  obstime=x['date']) for i in range(len(x['lon2 [arcsec]']))], axis=1)
     for i in range(len(df)):
-        # get the great arc for each pair of footpoints
-        ctilt=[]
-        for j in range(len(df['fp1'][i])):
-            p0= df['fp1'][i][j]
-            p1= df['fp2'][i][j]
-            p2= df['fp4'][i][j]
-            a=GreatArc(p1, p2, points=3)
-            b=GreatArc(p0, p2, points=3)
-            c=GreatArc(p0, p1, points=3)
-            a_ang = float(a.inner_angles()[2].value)
-            b_ang = float(b.inner_angles()[2].value)
-            c_ang = float(c.inner_angles()[2].value)
-            pair_tilt = np.rad2deg(np.arccos((np.cos(a_ang) - np.cos(b_ang) * np.cos(c_ang)) / (np.sin(b_ang) * np.sin(c_ang))))
-            if df['lat2 [arcsec]'][i][j] > df['lat1 [arcsec]'][i][j]:
-                ctilt.append(pair_tilt+90)
-            else:
-                ctilt.append(-pair_tilt+90)
+        p0= df['fp1'][i]
+        # fits a line to lat and lon and gets the tilt from the line slope
+        lat =  np.array([p.lat.arcsec for p in p0])
+        lon =  np.array([p.lon.arcsec for p in p0])
+        z = np.polyfit(lon, lat, 1)
+        tilt_p0 = np.arctan(z[0])*180/np.pi
+        # same for p1
+        p1= df['fp2'][i]
+        lat =  np.array([p.lat.arcsec for p in p1])
+        lon =  np.array([p.lon.arcsec for p in p1])
+        z = np.polyfit(lon, lat, 1)
+        tilt_p1 = np.arctan(z[0])*180/np.pi        
+        # same for p2
+        p2= df['fp4'][i]
+        lat =  np.array([p.lat.arcsec for p in p2])
+        lon =  np.array([p.lon.arcsec for p in p2])
+        z = np.polyfit(lon, lat, 1)
+        tilt_p2 = np.arctan(z[0])*180/np.pi
+        # computes the tilt angle as the mean of the three angles
+        ctilt = np.array([tilt_p0, tilt_p1, tilt_p2])
         df['tilt mean [deg]'][i] = np.mean(ctilt)
         df['tilt sd [deg]'][i] = np.std(ctilt)        
-
 
     ### PLOTS
 

tools/iglesias/compare_all_prop.py
@@ -14,6 +14,7 @@ from turtle import color, title
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+mpl.use('Agg')
 import os
 from astropy.coordinates import SkyCoord
 import astropy.units as u
@@ -21,23 +22,19 @@ import pandas as pd
 import datetime as dt
 from scipy.io import readsav
 
+
 # aux functions
-
-
 def correct_ang(iang, flip=0):
-    # corrects the input angle to be in the range -0 to 180 only
+    # corrects the input angle to be in the range -90 to 90 only
     # param: ang: input angles in deg (numpy array)
     ang = np.array(iang)
-    ang[iang > 270] -= 360
-    ang[iang > 180] -= 180
-    ang[iang > 90] -= 180
-    ang[iang < -270] += 360
-    ang[iang < -180] += 180
-    ang[iang < -90] += 180
-    if flip == 1:
-        ang[np.abs(ang) > 75] *= -1
-    #ang[ang < 0] += 180
-    return np.abs(ang)
+    ang[(ang < 20)] += 180
+    # if flip == 1:
+    #     ang[(ang < 0)] += 180
+    # else:
+    #     ang[(ang > 90) & (ang <= 270) ] -= 180
+    #     ang[(ang > 270)] -= 360 
+    return ang
 
 
 # main
@@ -101,31 +98,36 @@ gcs_keys = bla.sgui.dtype.names  # all keys in sav files
 os.makedirs(opath, exist_ok=True)
 exit
 # tilt angles
+df_ar['datetimes'] = pd.to_datetime(df_ar['Date'], format='%d/%m/%Y')
 gcs_vs_ar_val = []
 gcs_vs_fil_all = []
 gcs_vs_arcades_all = []
+diff_all = [] # last gcs tilt - mean arcade tilt
+diff_dates = []
+diff_gcs_param_all = []
 for d in df_fil['date']:
     d_str = d.strftime('%Y%m%d')
+    print('Plotting tilt angles for date...' + d_str)
     if d_str in gcs_dates:
         if d_str in sign_corr_dates:
             sign_corr = sign_corr_dates[d_str]
         else:
             sign_corr = [0,0,0]
-        gcs_key = 'ROT'
+        gcs_key = 'ROT'           
         gcs_idx = gcs_dates.index(d_str)
         gcs_val = np.rad2deg([float(sav.sgui[gcs_key]) for sav in gcs[gcs_idx]]).astype(float)
         gcs_times = np.array([dt.datetime.strptime(sav.sgui['ERUPTIONDATE'][0].decode('UTF-8'),
                                                    '%Y-%m-%dT%H:%M:%S.%f') for sav in gcs[gcs_idx]])
         ind = np.argsort(gcs_times)
         gcs_times = gcs_times[ind]
-        gcs_val = gcs_val[ind]
-        gcs_val = correct_ang(gcs_val, flip=sign_corr[0])
-        plt.plot(gcs_times, gcs_val, '*k', label='GCS')
+        gcs_val = gcs_val[ind]         
+        gcs_val = correct_ang(gcs_val, flip=1)
+        plt.plot(gcs_times, gcs_val, '*k', label='GCS')  
         x1 = d.to_pydatetime()
-        y1 = df_fil.loc[df_fil['date'] == d, 'tilt_angle']
+        y1 = df_fil.loc[df_fil['date'] == d, 'tilt_angle']         
         if len(y1) > 0:
             y1 = np.array(y1).astype(float)[0]
-            y1 = correct_ang(y1, flip=sign_corr[1])
+            y1 = correct_ang(y1, flip=1)
             plt.plot(x1, y1, 'sk', label='FIL')
             gcs_vs_fil_all.append([gcs_val[0], y1])
             gcs_times = np.concatenate((gcs_times, x1), axis=None)
@@ -134,30 +136,40 @@ for d in df_fil['date']:
         y2 = df_ar.loc[df_ar['ar_time_tilt'].dt.date == d.date(), 'ar_tilt']
         if len(y2) > 0:
             y2 = np.array(y2).astype(float)[0]
-            y2 = correct_ang(y2, flip=sign_corr[2])
-            plt.plot(x2, y2, 'ok', label='AR')
+            y2 = correct_ang(y2, flip=1)
+            plt.plot(x2, y2, '^k', label='AR')
             gcs_vs_ar_val.append([gcs_val[0], y2])
             gcs_times = np.concatenate((gcs_times, [i.to_pydatetime() for i in x2]), axis=None)
-            gcs_val = np.concatenate((gcs_val, y2), axis=None)
+            gcs_val = np.concatenate((gcs_val, y2), axis=None)       
         #adds arcade mean titls
         x3 = df_arcades.loc[df_arcades['date'].dt.date == d.date(), 'date']
         y3 = df_arcades.loc[df_arcades['date'].dt.date == d.date(), 'tilt mean [deg]']
         # subtracts 180 if y3 is larger than 90
         y3 = np.array(y3).astype(float)
         y3 = correct_ang(y3, flip=0)
+        # gcs rotation vs arcade
+        cdiff = float(np.abs(gcs_val[-1] - np.mean(y3)))
+        if cdiff > 90:
+            cdiff= 180 - cdiff
+        diff_all.append(cdiff)
+        diff_dates.append(d)
+        gcs_param = np.array((df_ar.loc[df_ar['datetimes'].dt.date == d.date(), 'gcs_lat_vel']))
+        if len(gcs_param) > 0:
+            gcs_param = np.nanmean(gcs_param)
+        diff_gcs_param_all.append(float(gcs_param))
         plt.plot(x3, y3, 'ok', label='Arcades')
         gcs_vs_arcades_all.append([gcs_val[0], y3[0]])
         ind = np.argsort(gcs_times)
         gcs_times = gcs_times[ind]
         gcs_val = gcs_val[ind]
-        plt.plot(gcs_times, gcs_val, '--k')
+        #plt.plot(gcs_times, gcs_val, '--k')
         plt.title(str(d))
         plt.ylabel('Tilt angle [deg]')
         plt.xlabel('Date')        
         plt.legend()
         plt.tight_layout()
         plt.minorticks_on()
-        plt.ylim([-180., 180.])
+        #plt.ylim([-180., 180.])
         plt.tick_params('both', which='both')
         plt.grid(which='both')
         plt.savefig(opath+'/'+d_str+'_'+gcs_key+'.png')
@@ -171,14 +183,19 @@ plt.scatter(gcs_vs_ar_val[:, 0], gcs_vs_ar_val[:, 1], color='r', label='gcs vs a
 plt.scatter(gcs_vs_arcades_all[:, 0], gcs_vs_arcades_all[:, 1], color='g', label='gcs vs arcades')
 plt.ylabel('fil/ar [deg]')
 plt.xlabel('gcs [deg]')  
-plt.xlim([-90, 90])
-plt.ylim([-90, 90])
+#plt.xlim([-90, 90])
+#plt.ylim([-90, 90])
 plt.legend()
-plt.savefig(opath+'/scatter.png')
+plt.savefig(opath+'/gcs_tilt_vs_fil-ar-arcade.png')
 plt.close()
 
+#plots diff vs date
+diff_all = np.array(diff_all)
+plt.scatter(diff_gcs_param_all, diff_all)
+plt.savefig(opath+'/diff_titl_gcs_vs_gcs_param.png')
+
 # plots df_ar['gcs_axial_vel'] vs df_arcades['width fit vel [km/s]'] for each date
-df_ar['datetimes'] = pd.to_datetime(df_ar['Date'], format='%d/%m/%Y')
+print('Plotting velocities...')
 all_x=[]
 all_y=[]
 for d in df_ar['datetimes']: